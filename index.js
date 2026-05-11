dc5795a1c580d36915c96f248426b5a94f8b89e8
johnsonjrf <johnsonoyetunji@gmail.com>
2026-05-11 23:57:02 +0300

    moved the secret to env file

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const cors = require("cors");
 const route = require("./routes/userRoutes");
 
 
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 const port = process.env.PORT||8080;
 
-const live_url = "mongodb+srv://JohnsonJrf:Remixjrf1@cluster0.skjluxr.mongodb.net/todoDB?appName=Cluster0"
+const live_url = process.env.MONGO_URI
 
                     mongoose.connect(live_url)
                     .then(() => console.log("connected to database"))
